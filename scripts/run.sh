401f869db34fc703315c1a245ea7fd3c3fae17b3
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-01-29 13:16:32 +0100

    example

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "current directory:"
+echo $(pwd)
+echo "running python src/dostuff.py"
+python src/dostuff.py
+
+echo "running python src/scripts/dostuff.py"
+python src/scripts/dostuff.py
---
+++ src/scripts/dostuff.py
@@ -0,0 +1,9 @@
+from util import strfunc
+
+
+def main():
+    print(strfunc.capitalize("Hello, world!"))
+
+
+if __name__ == '__main__':
+    main()
